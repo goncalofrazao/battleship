c53a0c065225ef7087342e2bf985e5d1fc1b6319
“madalenarb” <mariamadalenarob@tecnico.ulisboa.pt>
2021-03-21 19:53:40 +0000

    global id function has been finished

--- pieces.c
+++ pieces.c
@@ -1,26 +1,38 @@
 #include "headers.h"
 #include "pieces.h"
 
+//Esta função irá returnar o identificador global das peças
+
 int global_id_returner(int type_id, int instance_id)
 {
     int global_id;
     switch (type_id)
     {
-    case 1:
-        switch (instance_id)
-        {
         case 1:
-            global_id = 1;
+            global_id = instance_id;    
             break;
 
         case 2:
-            global_id = 2;
-            break;
+            global_id = instance_id + 9;
+
+        case 3:
+            global_id = instance_id + 21;
+
+        case 4:
+            global_id = instance_id + 27;
+
+        case 5:
+            global_id = instance_id + 31;
+
+        case 6:
+            global_id = instance_id + 35;
+
+        case 7:
+            global_id = instance_id + 39;
         
-        break;
+        case 8:
+            global_id = 42;
 
-
-        }
     }
     return global_id;
 }
