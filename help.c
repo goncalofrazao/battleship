fe7e182ef2e18adbe06509ca80814c4cfd1f8df8
StrangeRabbit <valorantbeastnew@gmail.com>
2021-03-27 18:01:52 +0000

    (help.c): corrected desconfiguration

--- help.c
+++ help.c
@@ -3,8 +3,8 @@
 
 void help_message() {
 	printf("This is a help message\
-            -h							show help message
-				-j							board dimensions (lines x column)\
+            -h							show help message\
+			-j							board dimensions (lines x column)\
             \
             \
             \
