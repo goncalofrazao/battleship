6b3f9f8eb1302d168c8a34e83a1c33b1585d626a
Tomás Amoroso <tomasamoroso@MBP-de-Tomas.lan>
2021-03-27 22:35:18 +0000

    (help.c) help message done

--- help.c
+++ help.c
@@ -19,11 +19,32 @@ void help_message() {
     -8 ----- number of pieces type 8\n\
     Usage:\n\
     -t <LxC> ----- L = lines number / C = columns number\n\
-    -j <num> ----- num = game mode\n\
+    -j <num> ----- num = game mode (integer)\n\
+    -p <num> ----- num = positioning mode\n\
+	 -d <num> ----- num = shotting mode\n\
+	 -1 <num> ----- num = number of pieces type 1\n\
+	 -2 <num> ----- num = number of pieces type 2\n\
+	 -3 <num> ----- num = number of pieces type 3\n\
+	 -4 <num> ----- num = number of pueces type 4\n\
+	 -5 <num> ----- num = number of pieces type 5\n\
+	 -6 <num> ----- num = number of pieces type 6\n\
+	 -7 <num> ----- num = number of pieces type 7\n\
+	 -8 <num> ----- num = number of pieces type 8\n\
     \n\
-    -2 <num> ----- num = numberof pieces type 2\n\
+
     Rules:\n\
-    -t <arg> ----- arg must be and integer between 0 and 2\n\
+    -t <arg> ----- arg must be an integer between lines and columns\n\
+	 -j <arg> ----- arg must be an integer between 0 and 2\n\
+	 -p <arg> ----- arg must be an integer between 1 and 2\n\
+	 -d <arg> ----- arg must be an integer between 1 and 3\n\
+	 -1 <arg> ----- arg must be an integer minimum 1\n\
+	 -2 <arg> ----- arg must be an integer
+	 -3 <arg> ----- arg must be an integer
+	 -4 <arg> ----- arg must be an integer
+	 -5 <arg> ----- arg must be an integer
+	 -6 <arg> ----- arg must be an integer
+	 -7 <arg> ----- arg must be an integer
+	 -8 <arg> ----- arg must be an integer
     \n\
     \n");
 }
