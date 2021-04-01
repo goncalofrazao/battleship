f96457d325c19bbeebc159fe1835512f4b5449ba
StrangeRabbit <valorantbeastnew@gmail.com>
2021-04-01 21:21:25 +0100

    (board.c): added columns and lines identifiers in board printer

--- board.c
+++ board.c
@@ -4,13 +4,27 @@
 void board_printer(int board[17][26], int lines, int columns)
 {
     for (int i = 1; i <= lines; i++){
-        for (int j = 1; j <= columns; j++)
-            if (board[i][j] > 0)
+        for (int j = 0; j <= columns; j++){
+            if (j == 0){
+                if (i < 10)
+                    printf(" %d ",i);
+                else
+                    printf("%d ", i);
+            }
+            else if (board[i][j] > 0)
                 printf( "%d ", board[i][j]);
             else
                 printf("- ");
+        }
         printf("\n");
     }
+    for (int i = 0; i < columns; i++){
+        if (i < 1)
+            printf("   ");
+        else 
+            printf("%c ", i + 'A');
+    }
+    printf("\n");
 }
 
 void clear_board(int board[17][26], int lines, int columns)
