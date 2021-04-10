46e1624668c64acd7cacc8d0e8d04963c4f96d1f
Gonçalo Frazão <gonfrazao@gmail.com>
2021-04-10 10:52:14 +0100

    Update pieces.c
    simplified global id returner

--- pieces.c
+++ pieces.c
@@ -310,42 +310,33 @@ identificador da peça e da variante
 
 int global_id_returner(int type_id, int instance_id)
 {
-    int global_id = 0;
     switch (type_id)
     {
         case 1:
-            global_id = instance_id;    
-            break;
+            return instance_id;    
 
         case 2:
-            global_id = instance_id + 9;
-            break;
+            return instance_id + 9;
 
         case 3:
-            global_id = instance_id + 21;
-            break;
+            return instance_id + 21;
 
         case 4:
-            global_id = instance_id + 27;
-            break;
+            return instance_id + 27;
 
         case 5:
-            global_id = instance_id + 31;
-            break;
+            return instance_id + 31;
 
         case 6:
-            global_id = instance_id + 35;
-            break;
+            return instance_id + 35;
 
         case 7:
-            global_id = instance_id + 39;
-            break;
+            return instance_id + 39;
 
         case 8:
-            global_id = 42;
-            break;
+            return 42;
     }
-    return global_id;
+    return 0;
 }
 
 
