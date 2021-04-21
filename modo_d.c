b25811849a9d30de57b2ec0de49454a306d997e8
Gonçalo Frazão <gonfrazao@gmail.com>
2021-04-21 13:31:15 +0100

    comments

--- modo_d.c
+++ modo_d.c
@@ -297,33 +297,36 @@ int end(int p_num[9], int p[9])
 * Function name: peca_killer_checker()
 *
 * Arguments: board[17][26] (int) - matrix of board
-*            x (int) - x of board position to check
-*            y (int) - y of board position to check
+*            x (int) - center x of square to check
+*            y (int) - center y of square to check
 *            p[9] (int) - number of pieces of each type already destroied
 *            type (int) - type of piece to check
 *
-* Return: -1 - if any pieces left to destroy
-*         1 - if all pieces destroied
+* Return: -1 - if the piece has not been destroied
+*         1 - if the piece has been destroied
 *
-* Side-effects: none
+* Side-effects: p[type] ++ if any piece destroied (type is the type of the piece
+*               in the square)
 *
-* Description: This function confirms if the destroied pieces correpond to the
-*              total pieces in the game.
+* Description: This function confirms if the piece in the square has been 
+*              destroied
 *
 *******************************************************************************/
 
 int peca_killer_checker(int board[17][26], int x, int y, int p[9], int type)
 {
+    // sum counts how many positions of the square had been shot
     int sum = 0;
     // x and y are in the middle of the board, so for start checking at y-1 and x-1
     for (int i = (y - 1); i < (y + 2); i++) {
         for (int j = (x - 1); j < (x + 2); j++){
-            // if board
+            // if board position is between 1 and 8 add 1 to positions already shot
             if (board[i][j] > 0 && board[i][j] < 9){
                 sum++;
             }
         }
     }
+    // if the number of positions is the same as the type of the piece => return 1 and add 1 to pieces destroied of the type
     if (sum == type && type != 0){
         p[type]++;
         return 1;
@@ -331,13 +334,33 @@ int peca_killer_checker(int board[17][26], int x, int y, int p[9], int type)
     return -1;
 }
 
+/*******************************************************************************
+* Function name: anulator()
+*
+* Arguments: board[17][26] (int) - matrix of board
+*            x (int) - center x of square to anulate
+*            y (int) - center y of square to anulate
+*
+* Return: no return
+*
+* Side-effects: all the positions around the piece destroied turn 9
+*
+* Description: This function rewrite all the positions around a piece already
+*              destroied to the value 9.
+*
+*******************************************************************************/
+
 void anulator(int board[17][26], int x, int y)
 {
+    // loops to go through all positions of a square
     for (int i = (y - 1); i < (y + 2); i++){
         for (int j = (x - 1); j < (x + 2); j++){
+            // if a positions in the square is between 1 and 8
             if (board[i][j] > 0 && board[i][j] < 9){
+                // run all positions around the position
                 for (int d = (i - 1); d < (i + 2); d++){
                     for (int e = (j - 1); e < (j + 2); e++){
+                        // if the position has not a number of a type, turn it to 9
                         if (board[d][e] < 1)
                             board[d][e] = 9;
                     }
@@ -347,35 +370,102 @@ void anulator(int board[17][26], int x, int y)
     }
 }
 
-int center(int pos)
+/*******************************************************************************
+* Function name: center()
+*
+* Arguments: position (int) - position to move to square center
+*
+* Return: (position + 1) - when (position % 3 = 1) the position is in the left 
+*         side of the square (if we talking about 'x') or is in the top
+*         of the square (if we talking about 'y')
+*           
+*         (position) - when (position % 3 = 2) the position is in the center 
+*         of the square (if we talking about 'x' or 'y)
+*
+*         (position - 1) - when (position % 3 = 0 (else) ) the position is in the right 
+*         side of the square (if we talking about 'x') or is in the bottom
+*         of the square (if we talking about 'y')
+*
+* Side-effects: no side effects
+*
+* Description: This function return the x or y coordinate of the center of the
+*              square that contains the position of input
+*
+*******************************************************************************/
+
+int center(int position)
 {
-    if (pos % 3 == 1)
-        return pos + 1;
-    if (pos % 3 == 2)
-        return pos;
-    if (pos % 3 == 0)
-        return pos - 1; 
-    return 0;
+    if (position % 3 == 1)
+        return position + 1;
+    else if (position % 3 == 2)
+        return position;
+    else
+        return position - 1; 
 }
+
+/*******************************************************************************
+* Function name: y_pc_to_user()
+*
+* Arguments: y (int) - y to convert to user read
+*            lines (int) - lines of the game
+*
+* Return: (lines + 1 - y) - always return this
+*
+* Side-effects: no side effects
+*
+* Description: This function converts the 'y' number that the computer uses to 
+*              the number that the player sees.
+*
+*******************************************************************************/
 
 int y_pc_to_user(int y, int lines)
 {
     return lines + 1 - y;
 }
 
+/*******************************************************************************
+* Function name: x_pc_to_user()
+*
+* Arguments: x (int) - x to convert to user read
+*
+* Return: (lines + 'A' - 1) - always return this
+*
+* Side-effects: no side effects
+*
+* Description: This function converts the 'x' number that the computer uses to 
+*              the number in ascii code that the player sees.
+*
+*******************************************************************************/
+
 int x_pc_to_user(int x)
 {
     return x + 'A' - 1;
 }
 
+/*******************************************************************************
+* Function name: pieces_declared()
+*
+* Arguments: p_num[9] (int) - pieces declared in the input
+*
+* Return: -1 - if no pieces declared
+*         1 - if there are pieces declared
+*
+* Side-effects: no side effects
+*
+* Description: This function converts the 'x' number that the computer uses to 
+*              the number in ascii code that the player sees.
+*
+*******************************************************************************/
+
 int pieces_declared(int p_num[9])
 {
-    // check if there are any pieces declared
     int i = 1;
     for (i = 1; i < 9; i++){
+        // if all the p_num positions are 0 i ends at 9
         if (p_num[i] != 0)
             break;
     }
+    // if i is bigger than 8 => none of p_num is different of 0 => no pieces to shoot 
     if (i > 8) {
         return -1;
     }
